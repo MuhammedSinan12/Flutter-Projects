28948d09a5cef0c7896b85fe745d1dc5b03a0797
MuhammedSinan12 <sinusinu1222@.com>
2022-09-20 15:50:28 +0530

    classcommit2

---
+++ bin/classdemo2.dart
@@ -0,0 +1,27 @@
+class Student {
+  String? name;
+  late int age;
+  int? phone;
+
+  static String course = "Flutter";
+}
+
+void main(List<String> args) {
+  Student student1 = Student();
+  print("Name of Student ${student1.name = "Abhi"}");
+  print("Age of Student ${student1.age = 20}");
+  print("Phone number is${student1.phone = 9645378282}");
+  print("Course is ${Student.course}");
+
+  Student student2 = Student();
+  print("Name of Student ${student2.name = "Anshi"}");
+  print("Age of Student${student2.age = 21}");
+  print("Phone number is ${student2.phone = 9641228282}");
+  print("Course is ${Student.course}");
+
+  Student student3 = Student();
+  print("Name of Student${student3.name = "Akhil"}");
+  print("Age of Student ${student3.age = 22}");
+  print("Phone number is${student3.phone = 9645378542}");
+  print("Course is ${Student.course}");
+}
