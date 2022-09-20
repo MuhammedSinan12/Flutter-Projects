3555b71e260e2cc8a565abf130a9d7c77713cdc4
MuhammedSinan12 <sinusinu1222@.com>
2022-09-20 15:49:30 +0530

    classcommit2

--- bin/classdemo.dart
+++ bin/classdemo.dart
@@ -16,5 +16,6 @@ void main(List<String> args) {
   print("Color is ${alto.color = "Green"}");
   print("Year of car is ${alto.year = 2010}");
   print("Mileage is ${alto.mileage = 15.6}");
+
   alto.show();
 }
