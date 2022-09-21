1b5962094d060d0a1099817e0cb92db22171222f
MuhammedSinan12 <sinusinu1222@.com>
2022-09-21 15:44:18 +0530

    6thday

---
+++ bin/parameterised2.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  student1('sinan', 22);
+
+  student2('Anshad', 22);
+}
+
+void student1(String name, int age) {
+  print('Name is $name');
+  print('Age is $age');
+}
+
+void student2(String name, int age) {
+  print('Name is $name');
+  print('Age is $age');
+}
