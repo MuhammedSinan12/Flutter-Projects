0a3970584ef8678549cd680bf195d392ee649605
MuhammedSinan12 <sinusinu1222@.com>
2022-09-21 15:55:33 +0530

    6thday

--- bin/parameterised2.dart
+++ bin/parameterised2.dart
@@ -2,6 +2,8 @@ void main(List<String> args) {
   student1('sinan', 22);
 
   student2('Anshad', 22);
+
+  showdetails("ismail", 22, 'ismailkhan@gmail', "Flutter");
 }
 
 void student1(String name, int age) {
@@ -12,4 +14,11 @@ void student1(String name, int age) {
 void student2(String name, int age) {
   print('Name is $name');
   print('Age is $age');
+}
+
+void showdetails(String name, int age1, String mail, String course) {
+  print(name);
+  print(age1);
+  print(mail);
+  print(course);
 }
