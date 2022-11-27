be2d885dbcc0e62d3be0c2ea690e8e53cc13ad90
MuhammedSinan12 <sinusinu1222@.com>
2022-11-27 18:04:11 +0530

    commit

---
+++ bin/assignments_dart/assignment1.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+void main() {
+  ///1
+  print('Odd numbers');
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 != 0) {
+      print('$i');
+    }
+  }
+
+  ///2
+  print('Even numbers');
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print('$i');
+    }
+  }
+
+  ///3
+  int sum = 0;
+  //sum of first 10 numbers
+  for (int i = 1; i <= 10; i++) {
+    sum = sum + i;
+  }
+
+  ///4
+  print('sum of 10 numbers:$sum');
+
+  int osum = 0, esum = 0;
+  for (int i = 1; i <= 10; i++) {
+    if (i % 2 != 0) {
+      osum = osum + i;
+    } else {
+      esum = esum + i;
+    }
+  }
+  print('Sum of odd numbers:$osum');
+  print('sum of even numbers:$esum');
+
+  ///5
+  int num;
+  print('Enter a number');
+  num = int.parse(stdin.readLineSync()!);
+  for (int i = 1; i <= 10; i++) {
+    print('$i * $num = ${i * num}');
+  }
+}
